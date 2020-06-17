c1b733564df1e0662df6fca420c4b186257eddff
JumarySanchez <sanchezjumary@gmail.com>
2020-06-17 13:03:28 -0400

    changed color deconstructor

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-// Your code here!
+const colorCombinator = require('./color-combinator.js')
+const colorDeconstructor = require('./color-deconstructor.js')
+
+const color1 = getInput(0) 
+const color2 = getInput(1)
+
+const outPut = colorCombinator(color1,color2)
+
+
+const color=getInput(0)
+const outPut1= colorDeconstructor(color)
+
+
+console.log(outPut)
+console.log(outPut1)
 
 
 
-// Our helper function here! Don't  touch!
 function getInput(i) {
   return process.argv[i + 2];
 }
