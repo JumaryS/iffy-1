29db007beba7e2f2518d9117c3e11cda46cb2634
JumarySanchez <sanchezjumary@gmail.com>
2020-06-17 13:54:42 -0400

    test

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const colorCombinator = require('./color-combinator.js')
-const colorDeconstructor = require('./color-decon.js')
+const colorDeconstructor = require('./color-deconstructor.js')
 
 const color1 = getInput(0) 
 const color2 = getInput(1)
@@ -11,17 +11,19 @@ const color2 = getInput(1)
 // const outPut1= colorDeconstructor(color)
 
 
-if(color1 === 'undefined'){
-  if(color2=== 'undefines'){
-
-  } return  " Please place in atleast one color"
+if(color1 === 'undefined' && color2 === 'undefined'){
+  return  " Please place in atleast one color"
+  // if(color2 === 'undefines'){
+  //   return  " Please place in atleast one color"
+  // } return  " Please place in atleast one color"
 }
 else if(color1 !== 'undefined' && color2 === 'undefined'){
   let colorDecon = colorDeconstructor
   console.log(colorDecon)
 }
-else if(color1 !== undefined && color2 !== undefined){
-  console.log(colorCombinator(color1,color2))
+else if(color1 !== 'undefined' && color2 !== 'undefined'){
+  let colors = colorCombinator
+  console.log(colors(color1,color2))
 }
 
 
